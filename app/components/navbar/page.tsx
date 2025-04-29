e9eed39249c438faa52aa55cb3e0c7dcf1f2ad5b
IvanMakevic <48869595+asdfmake@users.noreply.github.com>
2025-04-29 03:11:12 +0200

    Enabled auth and session for server and client Added mui components and setted them up.

---
+++ app/components/navbar/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useSession } from "next-auth/react"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+  const { data: session, status } = useSession()
+
+  if (status === "loading") return <p>Loading...</p>
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {session?.user?.name ? (
+            <p>Logged in as: {session.user.name}</p>
+          ) : (
+            <p>Not logged in</p>
+          )}
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
